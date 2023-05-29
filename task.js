9454dbd3b8a9eee58f9586ff0f670a72605baaf4
Sadia Bader <sadiabader433@gmail.com>
2023-05-29 19:10:18 +0500

    task 5 complete

--- task.js
+++ task.js
@@ -1,6 +1,6 @@
 //  Task-1
 
-let num = [2, 4, 5, 7, 9, 12, 20, 25, 33, 56, 87]
+let num = ["2", "4", "3", "7", "11", "12", "20", "25", "28", "57", "86"]
 
 function retrunEvenSum(num){
     let sum = 0;
@@ -12,6 +12,23 @@ for (i = 0; i < num.length; i++) {
 console.log(sum);
 }
 retrunEvenSum(num);
+
+// Task-2
+
+for(i = 0; i <= 100; i++){
+     
+if(i % 3 == 0 && i % 5 == 0){
+
+    console.log("FizzBuzz")
+}
+else if(i % 3 == 0){
+    console.log("Fizz")
+}
+
+else if(i % 5 == 0){
+    console.log("Buzz")
+}
+}
 
 // Task-3
 
@@ -31,8 +48,28 @@ printobj(data)
 
 // Task-4
 
-let alphabet = ["c", "a", "n", "h", "y","w",]
+let alphabet = ["c", "a", "n", "h", "y","w"]
 
 console.log(alphabet.sort());
 
 
+//  Task-5
+
+class employ {
+    constructor(name, id, subject, classs){
+        this.Name = name,
+        this.Id = id,
+        this.Subject = subject,
+        this.Classs = classs
+    }
+}
+class emp1 extends employ{
+    constructor(name, id, subject, classs){
+        super(name, id, subject, classs)   
+    }
+}
+let employs = new employ("Sana", 02, "Js", "c2");
+let empl = new emp1("Filza", 05, "PHp", "b2");
+
+console.log(employs)
+console.log(empl)
